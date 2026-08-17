2f515d6b5960ce30e73eeb9a5e68511553a73d79
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-08-17 14:27:35 +0530

    oop bacic

---
+++ OOP/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// In class variable and method store
+
+class Car
+{
+    // data store place ya blueprint.
+public:
+    int engineNo;
+    string modelName;
+    string color;
+    string type;
+    bool isEV;
+};
+
+int main()
+{
+    // car data
+    // int engineNo = 3;
+    // string modelname = "x1";
+    // string color = "red";
+    // string type = "suv";
+    // bool isEv = false;
+    // if car name ka variable multiple time uss kar sakte haii.
+    //like class is a image and object is a real photo
+    Car c1; // object make c1 => variale  .
+            // data storage format
+    c1.color = "red";
+    c1.engineNo = 133;
+    c1.isEV = false;
+    c1.modelName = "X1";
+    c1.type = "SUV";
+
+    // data print
+    cout << c1.engineNo;
+    return 0;
+}
