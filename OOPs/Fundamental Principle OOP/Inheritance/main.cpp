9131792e134c041c685797067a93f68ae2dbc4ee
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-08-26 23:49:49 +0530

    type of inheritance

--- OOPs/Fundamental Principle OOP/Inheritance/main.cpp
+++ OOPs/Fundamental Principle OOP/Inheritance/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 //1.simple inheritance
 //2.multilevel inheritance
 //3.multiple inheritance
+//4.hirechical inheritance
+//5. hyabrid inheritance
 int main(){
     return 0;
 }
