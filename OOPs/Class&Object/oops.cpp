dfa0d1e91f960706de466ca9333bd7218e8a8572
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-09-15 23:36:26 +0530

    ..

---
+++ OOPs/Class&Object/oops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Teachers{
+    //properties / atributes
+     string name ;
+     string dept;
+     string subject;
+     double salary;
+
+
+    // method / member function 
+     void changeDept(string newDept){
+        dept = newDept;
+
+     }
+};
+
+int main(){
+    Teachers 
+}
